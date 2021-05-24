f2b0836ebcc8edbf3f2f5dc9bbec1b5fb2a63222
clopezf12 <84690850+clopezf12@users.noreply.github.com>
2021-05-23 21:01:20 -0500

    Update Main.java

--- 1ProyectoGit/src/pkg1proyectogit/Main.java
+++ 1ProyectoGit/src/pkg1proyectogit/Main.java
@@ -17,6 +17,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mi nombre es Jhon Cuervo");
         
+        System.out.println("Hola soy Camiloooo");
     }
     
 }
